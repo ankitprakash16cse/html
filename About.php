d601d78c2b409abb445302c2f29d68c02349e7d6
Ankit Prakash Ching <65910349+ankitprakash16cse@users.noreply.github.com>
2024-02-12 11:19:02 +0530

    Add files via upload

---
+++ About.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="https://www.gla.ac.in/images/gla-university-logo.png" alt="">
+        <img src="https://www.gla.ac.in/images/naac-logo-1.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item" color="white">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/About.html">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Courses
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">BCA</a></li>
+            <li><a class="dropdown-item" href="#">MCA</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">B.tech</a></li>
+            <li><a class="dropdown-item" href="#">M.tech</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav><div class="bd-cheatsheet container-fluid bg-body">
+<div class="bd-example m-0 border-0">
+        <p class="h1">About GLA University</p>
+        </div>
+        <div class="bd-example-snippet bd-code-snippet"><div class="bd-example m-0 border-0">
+        <hr>
+        </div></div>
+        
+        <div class="container">
+		<div class="row">
+			<div class="col-md-12">
+	        	<div class="row">
+	<div class="col-md-3">
+		<img alt="ganeshi lal agrawal" class="img-fluid" src="https://www.gla.ac.in/images/ganeshi-lal-agrawal.jpg"> <strong class="pt-3 d-block text-center">Late Shri Ganeshi Lal Agrawal</strong></div>
+	<div class="col-md-9">
+		<h3>
+			I like the dreams of the future better than the history of the past.” -Thomas Jefferson</h3>
+		<p>
+			In 1991, Shri Narayan Das Agrawal decided to fulfill his father's dream, the Late Shri Ganeshi Lal Agrawal, of establishing an institute for quality education for the people and the region &amp; beyond. And initiated Sri Jagannath Prasad Ganeshi Lal Bazaz Charitable Trust Samiti to make the holy city Mathura a recognized destination for knowledge seekers from different spheres of life. This led to the foundation of a milestone at the karmabhoomi of the versatile and sagacious Lord Krishna.</p>
+	</div>
+</div>
+<div class="about-us-img">
+	<img class="img-fluid" src="https://www.gla.ac.in/images/about-us.jpg"></div>
+<p>
+	<span>This led to the foundation of a milestone at the karmabhoomi of the versatile and sagacious Lord Krishna. GLA University has been actively involved with social causes since its very inception and has drawn appreciation from one and all for its work in various facets of societal paradigms.</span></p>
+<div class="quality-p" id="aboutus-n">
+	<h4>
+		Quality Policy</h4>
+	<p>
+		To provide an empowering learning experience in established and emerging professions and capacity-building for leadership, entrepreneurship, and innovation.</p>
+</div>
+<div class="row">
+	<div class="col-md-3">
+		<img alt="ganeshi lal agrawal" class="img-fluid" src="/images/ganeshi-lal-agrawal.jpg" /><strong class="pt-3 d-block text-center">Late Shri Ganeshi Lal Agrawal</strong></div>
+	<div class="col-md-9">
+		<h3>
+			I like the dreams of the future better than the history of the past.&rdquo; - Thomas Jefferson</h3>
+		<p>
+			It was the year 1991 Shri Narayan Das Agrawal decided to fulfil the dream of his father, Late Shri Ganeshi Lal Agrawal, of establishing an institute for quality education to the people and the region &amp; beyond. and initiated Sri Jagannath Prasad Ganeshi Lal Bazaz Charitable Trust Samiti in an attempt to make the holy city Mathura a recognized destination for knowledge seekers from different spheres of life. This is what led to the foundation of a milestone at the karmabhoomi of the versatile and sagacious Lord Krishna.</p>
+	</div>
+</div>
+<div class="about-us-img">
+	<img class="img-fluid" src="/images/about-us.jpg" /></div>
+<p>
+	<span>This is what led to the foundation of a milestone at the karmabhoomi of the versatile and sagacious Lord Krishna. GLA University has been actively involved with social causes since its very inception and has drawn appreciation from one and all for its work in various facets of societal paradigms. </span></p>
+<div class="quality-p" id="aboutus-n">
+	<h4>
+		Quality Policy</h4>
+	<p>
+		To provide an empowering learning experience in established and emerging professions and capacity-building for leadership, entrepreneurship and innovation.</p>
+</div>
+			</div>
+		</div>
+	</div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ bootstrap.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+      <div class="inner-head fixed-top">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="https://www.gla.ac.in/images/gla-university-logo.png" alt="">
+        <img src="https://www.gla.ac.in/images/naac-logo-1.png" alt=""></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item" color="white">
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/form/About.php">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Courses
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">BCA</a></li>
+            <li><a class="dropdown-item" href="#">MCA</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">B.tech</a></li>
+            <li><a class="dropdown-item" href="#">M.tech</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav></div>
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.gla.ac.in/Uploads/banner/85hbanner_Ranking-banner.webp" width="400"
+    height="600" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Welcome to GLA University</h2>
+        <p>Let's explore the universe here!!!</p>
+        <button type="button" class="btn btn-primary">Courses</button>
+<button type="button" class="btn btn-secondary">Placement</button>
+<button type="button" class="btn btn-success">Achivement</button>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.gla.ac.in/Uploads/banner/89hbanner_banner-10.webp" width="400"
+    height="600" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://www.gla.ac.in/images/libraries-banner.jpg" width="400"
+    height="400" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<div class="container my-4">
+<div class="row mb-2">
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
+          <h3 class="mb-0">Featured post</h3>
+          <div class="mb-1 text-body-secondary">Nov 12</div>
+          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+            Continue reading
+            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <img src="https://www.gla.ac.in/images/innovation1.webp" width="200" height="250"  alt="">
+        </div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
+          <h3 class="mb-0">Post title</h3>
+          <div class="mb-1 text-body-secondary">Nov 11</div>
+          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+            Continue reading
+            <svg class="bi"><use xlink:href="#chevron-right"></use></svg>
+          </a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <img src="https://www.pagalguy.com/wp-content/uploads/2018/10/197D781B0C43AF09.jpg" width="200" height="250" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+
+</body>
+</html>
